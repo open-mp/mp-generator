253d5b6ed7d5cb9388ad65727ec02e2b392e9a7a
tsxuehu <tsxuehu@gmail.com>
2018-08-23 14:36:53 +0800

    init

--- src/generator/page/page-wxss.js
+++ src/generator/page/page-wxss.js
@@ -1,6 +1,6 @@
 
 
-exports.generate = async function (pageDir, mpDefinition) {
+exports.generate = async function (pageDir, pageDefinition) {
 
     let tpl = await fs.readFile(path.resolve(__dirname, 'tpl/index.wxss'));
     await fs.writeFile(path.resolve(pageDir, 'index.wxss'), tpl);
